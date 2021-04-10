1c7935f7c8133d8c4c8d3f7ebc55a8bcb4220c9f
Jorge <ezpadaz@me.com>
2021-04-10 18:52:31 -0500

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainLoader());
+}
+
+class MainLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // Esto estara lleno despues, solo es para iniciar el nuevo respositorio.
+        );
+  }
+}
