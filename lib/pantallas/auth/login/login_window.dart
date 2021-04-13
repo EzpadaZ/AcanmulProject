8f9a5b2fef93e4d691d8c79b65139ddf184199e7
Jorge <ezpadaz@me.com>
2021-04-13 17:47:13 -0500

    Added some components, initial design is somewhat done.

--- lib/pantallas/auth/login/login_window.dart
+++ lib/pantallas/auth/login/login_window.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../../backend/APIService.dart';
+import '../../../componentes/reusable_card.dart';
+import '../../../componentes/round_icon_button.dart';
 
 class LoginWindow extends StatefulWidget {
   @override
@@ -20,43 +22,68 @@ class _LoginWindowState extends State<LoginWindow> {
     return Scaffold(
         body: SafeArea(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Text(
-              "Login",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          SizedBox(height: size.height * 0.03),
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(horizontal: 40),
-            child: TextField(
-              decoration: InputDecoration(labelText: "E-Mail"),
-            ),
-          ), // EMAIL-CONTENEDOR.
+        children: [
           SizedBox(
-            height: size.height * 0.03,
+            height: size.height * 0.125,
           ),
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(horizontal: 40),
-            child: TextField(
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
+          ReusableCard(
+            color: Colors.grey.shade300,
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 40.0),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36,
+                          color: Colors.black87),
+                      textAlign: TextAlign.left,
+                    )),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextField(
+                    decoration: InputDecoration(labelText: "E-Mail"),
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 40),
+                  child: TextField(
+                    decoration: InputDecoration(labelText: "Password"),
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: RoundIconButton(
+                    labelTxt: "Iniciar Sesion",
+                    onpress: () => {
+                      // nada aun.
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                )
+              ],
             ),
-          ), // CONTRASENA CONTENEDOR.
-          SizedBox(
-            height: size.height * 0.5,
           ),
-          Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-          )
         ],
       ),
     ));
