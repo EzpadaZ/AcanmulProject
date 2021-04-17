fc505be41f58eca398d7d97fa9264b95f4aa5113
Jorge <ezpadaz@me.com>
2021-04-17 17:55:46 -0500

    Map Screen Works, Backend work needed next.

--- lib/pantallas/home/home_screen.dart
+++ lib/pantallas/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:acanmul_app/backend/APIService.dart';
 import 'package:flutter/material.dart';
+import '../gms/map_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,6 +12,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
       body: SafeArea(
@@ -22,7 +25,16 @@ class _HomeScreenState extends State<HomeScreen> {
                 AuthService.removeToken();
                 Navigator.popAndPushNamed(context, '/auth/login');
               },
-            )
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            GestureDetector(
+                child: Text('Map Demo'),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MapScreen()));
+                })
           ],
         ),
       ),
