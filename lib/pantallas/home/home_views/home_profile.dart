1507f619548b5ff4901b79669987dd39ecea2ee7
eduardo280198 <eduardo_280198@hotmail.com>
2021-04-23 23:39:55 -0500

    Cambios Perfil

--- lib/pantallas/home/home_views/home_profile.dart
+++ lib/pantallas/home/home_views/home_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:acanmul_app/componentes/datos_perfil.dart';
 
 class ProfileView extends StatefulWidget {
   @override
@@ -10,12 +11,28 @@ class ProfileView extends StatefulWidget {
 class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Text('Mi Perfil',
+    return Column(
+      //mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+
+        Container(
+          alignment:AlignmentDirectional.topStart,
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+        
+            child: Text('Mi Perfil',
             style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30.0)),
+        ),
       ),
+
+        DatosPerfil(label: 'Nombre:', informacion: 'Juanito',),
+        DatosPerfil(label: 'Email:', informacion: 'example@me.com',)
+        
+      ],
+
     );
+    
+    
+    
   }
 }
