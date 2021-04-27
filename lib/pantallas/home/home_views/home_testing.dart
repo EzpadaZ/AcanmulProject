43da9f206ba58681bde1837633196e9a97abe62c
Jose Damian <xjosdaminx@gmail.com>
2021-04-26 20:07:19 -0500

    acanmul

--- lib/pantallas/home/home_views/home_testing.dart
+++ lib/pantallas/home/home_views/home_testing.dart
@@ -1,6 +1,7 @@
 import 'package:acanmul_app/backend/modelos/Paquetes/Images.dart';
 import 'package:flutter/material.dart';
 import 'package:acanmul_app/backend/modelos/Paquetes/Paquete.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class TestingView extends StatelessWidget {
   List<Images> imagenes = [];
@@ -9,22 +10,72 @@ class TestingView extends StatelessWidget {
   TestingView({this.paquete});
 
   void prepararDemo() {
-    imagenes.add(Images(url: 'aaaaaaa'));
-    imagenes.add(Images(url: 'aaaaaaa'));
-    imagenes.add(Images(url: 'aaaaaaa'));
+    imagenes.add(Images(url: 'https://culturacampeche.com/turismocultural/images/municipios/calakmul/cal.JPG'));
+    imagenes.add(Images(url: 'https://mexicotravelchannel.com.mx/wp-content/uploads/2021/01/calakmul-sigue-las-rutas-selvaticas-de-este-destino-en-campeche-mexico-travel-channel.jpg'));
+    imagenes.add(Images(url: 'https://www.inah.gob.mx/images/fotodeldia/20180919_calakmul.jpg'));
     paquete = Paquete(
-        id: 'a',
-        titulo: 'titulo',
+        id: 'M474M3Y4',
+        titulo: 'Calakmul',
         descripcion: 'aaa',
         images: imagenes,
-        ubicaciones: ['1', '2']);
+        ubicaciones: ['ubi1', 'ubi2']);
   }
 
+//child: Text(paquete.titulo),
   @override
   Widget build(BuildContext context) {
     prepararDemo();
     return Container(
-      child: Text(paquete.titulo),
+      child: CarouselSlider(
+        options: CarouselOptions(
+          aspectRatio: 2.0,
+          viewportFraction: 1,
+          enlargeCenterPage: true,
+          enableInfiniteScroll: false,
+          initialPage: 2,
+          autoPlay: true,
+        ),
+        items: imageSliders,),
     );
   }
+
+  final List<Widget> imageSliders = .map((item) => Container(
+  child: Container(
+    margin: EdgeInsets.all(5.0),
+    child: ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      child: Stack(
+        children: <Widget>[
+          Image.network(item, fit: BoxFit.cover, width: 1200.0),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(200, 0, 0, 0),
+                    Color.fromARGB(0, 0, 0, 0)
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                'LugarX. Imagen ${imagenes.indexOf(item)}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+    ),
+  ),
+)).toList();
 }
