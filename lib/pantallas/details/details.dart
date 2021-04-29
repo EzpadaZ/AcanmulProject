6469be56b7899bc306c4677a833a63f91682f539
Jose Damian <xjosdaminx@gmail.com>
2021-04-28 21:46:03 -0500

    Merge branch 'Testing' of https://github.com/JorgeEspadas/AcanmulProject into Testing

--- lib/pantallas/details/details.dart
+++ lib/pantallas/details/details.dart
@@ -2,27 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:acanmul_app/backend/modelos/Paquetes/Images.dart';
 import 'package:acanmul_app/backend/modelos/Paquetes/Paquete.dart';
 import 'package:acanmul_app/componentes/tempLugares.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class DetailsView extends StatelessWidget {
   List<Images> imagenes = [];
   Paquete paquete;
+  List<Widget> imageSlider;
 
   DetailsView({this.paquete});
 
   void prepararDemo() {
-    imagenes.add(Images(url: 'aaaaaaa'));
-    imagenes.add(Images(url: 'aaaaaaa'));
-    imagenes.add(Images(url: 'aaaaaaa'));
+    imagenes.add(Images(
+        url:
+            'https://culturacampeche.com/turismocultural/images/municipios/calakmul/cal.JPG'));
+    imagenes.add(Images(
+        url:
+            'https://mexicotravelchannel.com.mx/wp-content/uploads/2021/01/calakmul-sigue-las-rutas-selvaticas-de-este-destino-en-campeche-mexico-travel-channel.jpg'));
+    imagenes.add(Images(
+        url:
+            'https://www.inah.gob.mx/images/fotodeldia/20180919_calakmul.jpg'));
+
     paquete = Paquete(
-        id: 'a',
-        titulo: 'titulo',
+        id: 'M474M3Y4',
+        titulo: 'Calakmul',
         descripcion: 'aaa',
         images: imagenes,
         ubicaciones: []);
   }
 
+    void inicializarSlider() {
+    imageSlider = paquete.images
+        .map((item) => Container(
+              child: Container(
+                margin: EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Image.network(item.url,
+                            fit: BoxFit.cover, width: 1200.0),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              ),
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            child: Text(
+                              '',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
+  }
+
   Widget build(BuildContext context) {
     prepararDemo();
+    inicializarSlider();
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -35,7 +90,17 @@ class DetailsView extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           SizedBox(height: 10),
-          Text("Aqui va el carrusel"),
+          CarouselSlider(
+            options: CarouselOptions(
+              aspectRatio: 2.0,
+              viewportFraction: 1,
+              enlargeCenterPage: true,
+              enableInfiniteScroll: false,
+              initialPage: 2,
+              autoPlay: true,
+            ),
+            items: imageSlider,
+          ),
           SizedBox(height: 20),
           ListView(
             padding: EdgeInsets.symmetric(horizontal: 20),
